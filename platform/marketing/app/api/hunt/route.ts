161e6d4c4762403cad10c5f91acd0336982e1d63
Arsen Shkrumelyak <arsen@nerverift.com>
2024-05-13 18:31:01 +0100

    update wrangler.toml and route.ts

--- platform/marketing/app/api/hunt/route.ts
+++ platform/marketing/app/api/hunt/route.ts
@@ -6,7 +6,11 @@ export async function POST(req: NextRequest) {
   const { text } = await req.json();
   const KAYLE_API_KEY = process.env.KAYLE_API_KEY!;
 
-  const response = await fetch("https://api.kayle.ai/v1/moderate", {
+  const engine = process.env.NODE_ENV === "production"
+  ? "https://api.kayle.ai/v1/moderate"
+  : "http://127.0.0.1:8787/v1/moderate";
+
+  const response = await fetch(engine, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
