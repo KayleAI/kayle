944015e583716cec634499a05ec24067e02bcddb
Arsen Shkrumelyak <arsen@nerverift.com>
2024-05-25 22:09:45 +0100

    update providers.tsx

--- platform/app/providers.tsx
+++ platform/app/providers.tsx
@@ -4,7 +4,11 @@ import { ThemeProvider } from 'next-themes'
 
 export function Providers({ children }: { readonly children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" disableTransitionOnChange>
+    <ThemeProvider
+      attribute="class"
+      disableTransitionOnChange
+      enableSystem
+    >
       {children}
     </ThemeProvider>
   )
