8f41772430dd1687e13e67bb123ab8a3809a183c
Arsen Shkrumelyak <arsen@nerverift.com>
2024-05-25 20:43:12 +0100

    delete 2 files and update 1 file

--- platform/middleware.ts
+++ platform/middleware.ts
@@ -1,10 +1,72 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
+import { type CookieOptions, createServerClient } from "@supabase/ssr";
+import { type NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  // TODO: If the user is not logged in, rewrite the URL to /home for marketing purposes
+  const baseUrl = new URL(request.nextUrl.origin);
 
-  return await updateSession(request);
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+        remove(name: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+        },
+      },
+    },
+  );
+
+  const { data: { user }, error } = await supabase.auth.getUser();
+
+  if (request.nextUrl.pathname === "/") {
+    baseUrl.pathname = "/home";
+    if (!error && user) {
+      baseUrl.pathname = "/dashboard";
+    }
+    response = NextResponse.rewrite(baseUrl.toString());
+  }
+
+  return response;
 }
 
 export const config = {
