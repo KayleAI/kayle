65733b7f42f9b955a0a2f9e2906b810789da279a
Arsen Shkrumelyak <arsen@nerverift.com>
2024-06-05 11:15:23 +0100

    update .npmrc, apps/discord/index.ts and tailwind.css

--- apps/discord/index.ts
+++ apps/discord/index.ts
@@ -1,7 +1,11 @@
+/**
+ * 
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
 require('dotenv').config();
 
 const token = process.env.DISCORD_BOT_SECRET;
 
-// TODO: Write code
+// Create a new Discord client... 
+
+*/
