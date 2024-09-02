312077b8fa6bc462d3378bc42945b210d3a8d088
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-09-02 16:02:23 +0100

    icons

---
+++ packages/icons/src/docs/index.ts
@@ -0,0 +1,3 @@
+// @ts-nocheck - we don't want to check this file for now
+
+// TODO: add new icons for the docs
--- platform/components/docs/icons/BellIcon.tsx
+++ platform/components/docs/icons/BellIcon.tsx
@@ -1,17 +1,34 @@
 export function BellIcon(props: React.ComponentPropsWithoutRef<"svg">) {
 	return (
-		<svg viewBox="0 0 20 20" aria-hidden="true" {...props}>
-			<path
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				d="M4.438 8.063a5.563 5.563 0 0 1 11.125 0v2.626c0 1.182.34 2.34.982 3.332L17.5 15.5h-15l.955-1.479c.641-.993.982-2.15.982-3.332V8.062Z"
-			/>
-			<path
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 18 18"
+			aria-hidden="true"
+			{...props}
+		>
+			<g
+				stroke-linecap="round"
+				stroke-width="1.5"
 				fill="none"
-				strokeLinecap="round"
+				stroke="currentColor"
 				strokeLinejoin="round"
-				d="M7.5 15.5v0a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v0"
-			/>
+			>
+				<path
+					d="M15.75 13.25C14.645 13.25 13.75 12.355 13.75 11.25V6.5C13.75 3.877 11.623 1.75 9 1.75C6.377 1.75 4.25 3.877 4.25 6.5V11.25C4.25 12.355 3.355 13.25 2.25 13.25H15.75Z"
+					fill="currentColor"
+					fillOpacity={0.3}
+					data-color="color-2"
+					data-stroke="none"
+					stroke="none"
+				/>
+				<path d="M15.75 13.25C14.645 13.25 13.75 12.355 13.75 11.25V6.5C13.75 3.877 11.623 1.75 9 1.75C6.377 1.75 4.25 3.877 4.25 6.5V11.25C4.25 12.355 3.355 13.25 2.25 13.25H15.75Z" />
+				<path
+					d="M10.588 15.185C10.493 15.068 10.351 15 10.2 15H7.801C7.65 15 7.508 15.068 7.413 15.185C7.318 15.302 7.281 15.456 7.312 15.603C7.485 16.425 8.18 17 9.001 17C9.822 17 10.517 16.425 10.69 15.603C10.721 15.456 10.683 15.302 10.588 15.185Z"
+					fill="currentColor"
+					data-stroke="none"
+					stroke="none"
+				/>
+			</g>
 		</svg>
 	);
 }
