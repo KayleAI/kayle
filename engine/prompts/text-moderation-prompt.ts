3f4c126f1f026dd36f3d983bd76169b92f0b83cb
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-09-07 12:31:47 +0100

    add fixed violations list

--- engine/prompts/text-moderation-prompt.ts
+++ engine/prompts/text-moderation-prompt.ts
@@ -4,6 +4,9 @@ import type { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 // Zod
 import { z } from "zod";
 
+// Config
+import { violations } from "@/config/violations";
+
 export const textModerationPrompt: ChatCompletionMessageParam = {
 	role: "system",
 	content:
@@ -17,10 +20,8 @@ export const textModerationResponseFormat = z.object({
 			"A number between 0 and 10 representing how severe the violation is. 0 means no violation, 10 means the violation is extremely severe.",
 		),
 	violations: z
-		.array(z.string())
-		.describe(
-			"A list of identifiers for violations (for example, 'hate', 'self-harm', 'pii', 'profanity', and so on)",
-		),
+		.array(z.enum(violations as [string, ...string[]]))
+		.describe("A list of identifiers for violations."),
 	pii: z
 		.array(z.string())
 		.optional()
